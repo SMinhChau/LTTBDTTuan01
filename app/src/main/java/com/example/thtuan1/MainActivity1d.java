4840d29a9cf6752b1d4252c234c2848bc183f1d9
SMinhChau <minhchaunguyenmtp@gmail.com>
2022-02-21 12:22:39 +0700

    finish all

---
+++ app/src/main/java/com/example/thtuan1/MainActivity1d.java
@@ -0,0 +1,14 @@
+package com.example.thtuan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity1d extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity1d);
+    }
+}
